ca321e93b4697acb1224d7180110b3938e0ac57d
Bob <bob_w@qq.com>
2021-08-09 10:39:41 +0800

    npm init

---
+++ Nest.ts
@@ -0,0 +1,15 @@
+import * as chalk from 'chalk';
+export default class Nest {
+
+}
+export function assert(desc: string, condition: boolean) {
+  if (condition) {
+    console.log(chalk.green(desc, ' passed'));
+  } else {
+    console.log(chalk.red(desc, ' error'));
+  }
+}
+export function describle(desc: string, callback: () => {}) {
+  console.log(chalk.blue(desc));
+  callback();
+}
